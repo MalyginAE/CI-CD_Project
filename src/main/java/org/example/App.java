0b760d80b5df582e9f8229437f0f6f4d6f2c29d2
Malygin, Andrey <Andrey.Malygin@nexign.com>
2022-06-15 17:13:26 +0300

    fix

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,22 +1,24 @@
 package org.example;
+
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.request.SendMessage;
 
-public class App
-{
+public class App {
     static String token = System.getProperty("token");
-static {
-    if (token == null) {
-        token = System.getenv("token");
+
+    static {
+        if (token == null) {
+            token = System.getenv("token");
+        }
     }
-}
+
     static TelegramBot bot = new TelegramBot(token);
     static String chatId = System.getenv("chatId");
-    public static void main( String[] args )
-    {
+
+    public static void main(String[] args) {
 
 
-        SendMessage sendMessage  = new SendMessage(chatId,"привет от CI/CD");
+        SendMessage sendMessage = new SendMessage(chatId, "привет от CI/CD");
         bot.execute(sendMessage);
     }
 }
